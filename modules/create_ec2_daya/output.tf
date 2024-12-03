0d9392041cad0ee3ba9328831d7a5937bced035f
Dayashankar28 <dayamm28@gmail.com>
2024-12-03 09:43:40 +0000

    sda

--- modules/create_ec2_daya/output.tf
+++ modules/create_ec2_daya/output.tf
@@ -1,11 +1,11 @@
-output "ami_id" {
-  value = data.aws_ami.ubuntu.id
-}
+# output "ami_id" {
+#   value = data.aws_ami.ubuntu.id
+# }
 
-output "security_groups" {
-    value = data.aws_security_group.default.name
+# output "security_groups" {
+#     value = data.aws_security_group.default.name
   
-}
+# }
 
 output "ec2_public_ip" {
   value = aws_instance.create_ec2.public_ip
